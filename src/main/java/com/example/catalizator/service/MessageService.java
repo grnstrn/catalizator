0ccace69c55ed267e4a21b99898ddbbc8a1f0994
grnstrn <starostingrigory@gmail.com>
2020-06-23 19:02:57 +0300

    webFlux

---
+++ src/main/java/com/example/catalizator/service/MessageService.java
@@ -0,0 +1,24 @@
+package com.example.catalizator.service;
+
+import com.example.catalizator.domain.Message;
+import com.example.catalizator.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MessageService {
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MessageService(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+    public Flux<Message> list(){
+        return messageRepo.findAll();
+    }
+    public Mono<Message> addOne (Message message){
+        return messageRepo.save(message);
+    }
+}
